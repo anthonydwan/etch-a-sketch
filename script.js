0e9ab4b913343664f3bcaa9c74c34ddbc37e17bd
anthony <anthonydwan@outlook.com>
2021-08-07 17:43:23 +1000

    starting the project

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector("#container")
+
+function makeDiv(){
+    for (let i = 0; i < 32*32;i++){
+        const square = document.createElement("div");
+        square.classList.add("squareDiv");
+        container.appendChild(square);
+    }
+}
+
+makeDiv()
+
+function draw(){
+    document.querySelector("#")
+    addEventListener
+}
