980a7f9617f7878c6da47568ecbc2ae0902f06ca
Anthony <anthonydwan@outlook.com>
2021-08-07 23:20:21 +1000

    finished basic functionality of the sketch

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const container = document.querySelector("#container")
+const clearButton = document.querySelector("#clearButton")
 
 function makeDiv(){
     for (let i = 0; i < 32*32;i++){
         const square = document.createElement("div");
         square.classList.add("squareDiv");
+        square.addEventListener("mousemove", drawnSquare)
         container.appendChild(square);
     }
 }
 
+function drawnSquare(){
+    this.classList.add("drawnBlack")
+}
+
 makeDiv()
 
-function draw(){
-    document.querySelector("#")
-    addEventListener
+function clearAll(){
+    const squares = document.querySelectorAll(".squareDiv");
+    for (square of squares){
+        square.classList.add("reset");
+        square.className = "squareDiv";
+    }
 }
+
+
+clearButton.addEventListener('click', clearAll)
+
