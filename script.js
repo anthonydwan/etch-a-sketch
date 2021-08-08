95ab82ede3d55ec7808ffb8d0d425ddecddd3802
Anthony <anthonydwan@outlook.com>
2021-08-08 13:34:11 +1000

    minor update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const container = document.querySelector("#container")
 const clearButton = document.querySelector("#clearButton")
 
-function makeDiv(){
-    for (let i = 0; i < 32*32;i++){
+function makeDiv() {
+    for (let i = 0; i < 32 * 32; i++) {
         const square = document.createElement("div");
         square.classList.add("squareDiv");
         square.addEventListener("mousemove", drawnSquare)
@@ -10,20 +10,23 @@ function makeDiv(){
     }
 }
 
-function drawnSquare(){
+function drawnSquare() {
     this.classList.add("drawnBlack")
 }
 
 makeDiv()
 
-function clearAll(){
+function clearAll() {
     const squares = document.querySelectorAll(".squareDiv");
-    for (square of squares){
-        square.classList.add("reset");
-        square.className = "squareDiv";
-    }
+    for (square of squares) {
+        timeoutReset(square)
+    };
 }
 
+function timeoutReset(square){
+    setTimeout(square.classList.add("reset"), 1000);
+    setTimeout(square.className = "squareDiv", 2000);
+};
 
 clearButton.addEventListener('click', clearAll)
 
