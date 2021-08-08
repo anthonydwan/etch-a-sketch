ac8c43c80d0f3623037d48ac68abb4a90f631f19
anthony <anthonydwan@outlook.com>
2021-08-08 17:00:53 +1000

    finished core drawing mechanics

--- script.js
+++ script.js
@@ -1,27 +1,72 @@
-const container = document.querySelector("#container")
-const clearButton = document.querySelector("#clearButton")
+const container = document.querySelector("#container");
+const clearButton = document.querySelector("#clearButton");
+const colorButton = document.querySelector("#colorButton");
+const blackButton = document.querySelector("#blackButton");
+const eraserButton = document.querySelector("#eraserButton");
+let currentColor = "black";
 
 function makeDiv(color) {
     for (let i = 0; i < 32 * 32; i++) {
         const square = document.createElement("div");
         square.classList.add("squareDiv");
-        square.addEventListener("mousemove", color, false)
+        square.addEventListener("mousemove", color)
         container.appendChild(square);
     }
 }
 
-function drawBlack() {
-    this.style.cssText = "background-color: black; transition: all 0.25s ease-in-out;"
+function draw(color) {
+    const squares = document.querySelectorAll(".squareDiv");
+    for (let i = 0; i < squares.length; i++) {
+        removePrevListener(squares[i])
+        squares[i].addEventListener("mousemove", color)
+    }
 }
+
+
 
 function clearAll() {
     const squares = document.querySelectorAll(".squareDiv");
-    for(let i = 0; i < squares.length; i++){
+    for (let i = 0; i < squares.length; i++) {
         squares[i].style.cssText = "background-color: white; transition: all 0.25s ease-in-out;"
     }
 }
 
-clearButton.addEventListener('click', clearAll)
+function removePrevListener(obj) {
+    switch (currentColor) {
+        case "black":
+            obj.removeEventListener("mousemove", makeBlack);
+            break;
+        case "color":
+            obj.removeEventListener("mousemove", makeRandColor);
+            break;
+        case "white":
+            obj.removeEventListener("mousemove", makeWhite);
+            break;
+    };
+};
 
+function makeBlack() {
+    this.style.cssText = "background-color: black; transition: all 0.25s ease-in-out;"
+    currentColor = "black"
+};
 
-makeDiv(drawBlack);
+function makeRandColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const color = `rgb(${r},${g}, ${b})`;
+    this.style.cssText = `background-color: ${color}; transition: all 0.25s ease-in-out;`
+    currentColor = "color";
+}
+
+function makeWhite() {
+    this.style.cssText = "background-color: white; transition: all 0.25s ease-in-out;"
+    currentColor = "white";
+}
+
+makeDiv(makeBlack);
+
+clearButton.addEventListener('click', clearAll);
+colorButton.addEventListener('click', () => draw(makeRandColor));
+eraserButton.addEventListener('click', () => draw(makeWhite));
+blackButton.addEventListener('click', () => draw(makeBlack));
