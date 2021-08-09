2260953a8bb51103cee96fd8021b6494ea5d4eca
anthony <anthonydwan@outlook.com>
2021-08-09 16:00:27 +1000

    fixed bug + adding pen

--- script.js
+++ script.js
@@ -1,72 +1,108 @@
 const container = document.querySelector("#container");
 const clearButton = document.querySelector("#clearButton");
-const colorButton = document.querySelector("#colorButton");
+const randColorButton = document.querySelector("#randColorButton");
 const blackButton = document.querySelector("#blackButton");
 const eraserButton = document.querySelector("#eraserButton");
+const originalColor = "rgb(240, 240, 240)";
+const eraserEnd = document.querySelector("#eraserEnd")
+
 let currentColor = "black";
 
-function makeDiv(color) {
-    for (let i = 0; i < 32 * 32; i++) {
+function makeDiv(color, colorHover) {
+    for (let i = 0; i < 64 * 88; i++) {
         const square = document.createElement("div");
         square.classList.add("squareDiv");
-        square.addEventListener("mousemove", color)
+        square.setAttribute('draggable', 'false');
+        square.addEventListener("mousedown", color);
+        square.addEventListener("mouseenter", colorHover);
         container.appendChild(square);
     }
 }
 
-function draw(color) {
+function draw(color, colorHover) {
     const squares = document.querySelectorAll(".squareDiv");
     for (let i = 0; i < squares.length; i++) {
         removePrevListener(squares[i])
-        squares[i].addEventListener("mousemove", color)
+        squares[i].addEventListener("mousedown", color);
+        squares[i].addEventListener("mouseenter", colorHover);
     }
 }
 
-
+function erase(){
+    eraserEnd.classList.add("eraserActivated");
+    draw(makeEraser, makeEraserHover);
+}
 
 function clearAll() {
     const squares = document.querySelectorAll(".squareDiv");
     for (let i = 0; i < squares.length; i++) {
-        squares[i].style.cssText = "background-color: white; transition: all 0.25s ease-in-out;"
+        squares[i].style.backgroundColor = originalColor;
     }
 }
 
 function removePrevListener(obj) {
     switch (currentColor) {
         case "black":
-            obj.removeEventListener("mousemove", makeBlack);
+            obj.removeEventListener("mousedown", makeBlack);
+            obj.removeEventListener("mouseenter", makeBlackHover);
             break;
         case "color":
-            obj.removeEventListener("mousemove", makeRandColor);
+            obj.removeEventListener("mousedown", makeRandColor);
+            obj.removeEventListener("mouseenter", makeRandColorHover);
             break;
-        case "white":
-            obj.removeEventListener("mousemove", makeWhite);
+        case "eraser":
+            obj.removeEventListener("mousedown", makeEraser);
+            obj.removeEventListener("mouseenter", makeEraserHover);
+            eraserEnd.classList.remove("eraserActivated")
             break;
     };
 };
 
-function makeBlack() {
-    this.style.cssText = "background-color: black; transition: all 0.25s ease-in-out;"
-    currentColor = "black"
+function makeBlack(e) {
+    e.preventDefault()
+    e.target.style.backgroundColor = "black";
+    currentColor = "black";
 };
 
-function makeRandColor() {
+function makeBlackHover(e) {
+    if (e.buttons > 0) {
+        makeBlack(e);
+    };
+};
+
+function makeRandColor(e) {
+    e.preventDefault()
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
     const color = `rgb(${r},${g}, ${b})`;
-    this.style.cssText = `background-color: ${color}; transition: all 0.25s ease-in-out;`
+    e.target.style.backgroundColor = `${color}`
     currentColor = "color";
-}
+};
 
-function makeWhite() {
-    this.style.cssText = "background-color: white; transition: all 0.25s ease-in-out;"
-    currentColor = "white";
-}
+function makeRandColorHover(e) {
+    if (e.buttons > 0) {
+        makeRandColor(e);
+    };
+};
 
-makeDiv(makeBlack);
+function makeEraser(e) {
+    e.preventDefault()
+    e.target.style.backgroundColor = originalColor;
+    currentColor = "eraser";
+};
+
+function makeEraserHover(e) {
+    if (e.buttons > 0) {
+        makeEraser(e);
+    };
+};
+
+makeDiv(makeBlack, makeBlackHover);
 
 clearButton.addEventListener('click', clearAll);
-colorButton.addEventListener('click', () => draw(makeRandColor));
-eraserButton.addEventListener('click', () => draw(makeWhite));
-blackButton.addEventListener('click', () => draw(makeBlack));
+randColorButton.addEventListener('click', () => draw(makeRandColor, makeRandColorHover));
+eraserButton.addEventListener('click', () => erase());
+blackButton.addEventListener('click', () => draw(makeBlack, makeBlackHover));
+
+
